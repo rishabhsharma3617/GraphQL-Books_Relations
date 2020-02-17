b0f80991472a5bbd2c496109beb4f60fa91028ca
Rishabh Sharma <rishabhsharma3617@gmail.com>
2020-02-17 23:49:53 +0530

    Added the Apoolo CLient dependencies and embedded them into the cde

--- project/client/src/App.js
+++ project/client/src/App.js
@@ -1,14 +1,24 @@
 import React,{Component} from 'react';
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from 'react-apollo' //it made the react interact with the apollo which helps to inject the data fetched into our react app
 //components
 import BookList from './components/BookList'
+
+const client = new ApolloClient({
+  uri : 'http://localhost:4000/graphql'   //where server is hosted, it is the endpoint whic is gonna give us the data
+})
 
 class App extends Component {
   render() {
     return (
+      <ApolloProvider client={client}>
+        
       <div id="main">
         <h1>Rishabh's reading list</h1>
         <BookList />
       </div>
+
+      </ApolloProvider>
     );
   }
 
