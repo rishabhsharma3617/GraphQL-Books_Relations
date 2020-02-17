e3cdba320bb7b6068df158efa3c3f54185b45865
Rishabh Sharma <rishabhsharma3617@gmail.com>
2020-02-17 19:46:14 +0530

    Made the server run

---
+++ project/server/app.js
@@ -0,0 +1,8 @@
+const express = require('express')
+
+
+const app = express()
+
+app.listen(3000, () => {
+    console.log("Server is successfully running on the port 3000 , Now make the Project")
+})
