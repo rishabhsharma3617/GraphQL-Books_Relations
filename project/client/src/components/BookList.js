7d2c16d5994a77cdb3abb56fd7fb4a018a160f3d
Rishabh Sharma <rishabhsharma3617@gmail.com>
2020-02-17 23:40:16 +0530

    Added the Booklist Component

---
+++ project/client/src/components/BookList.js
@@ -0,0 +1,18 @@
+import React , {Component} from 'react';
+
+
+class BookList extends Component{
+  render(){
+      
+    
+    return (
+    <div className="App">
+        <ul id="book-list">
+            <li>BookName</li>
+        </ul>
+    </div>
+          );
+      }
+}
+
+export default BookList;
